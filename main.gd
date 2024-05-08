21a507648bf100844f8ed415f67e9a4ac47daaf5
florin <apetreiflorin236@gmail.com>
2024-05-08 16:07:12 +0200

    tried the godot game engine

---
+++ main.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+func _ready():
+	pass
+
+func _process(delta):
+	pass
+
+func _on_quit_pressed():
+	get_tree().quit()
+
+func _on_play_pressed():
+	get_tree().change_scene_to_file("res://world.tscn")
